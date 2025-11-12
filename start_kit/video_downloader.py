0a21b189d9a53f782c1ff108278456d9df400117
zythedeveloper <zythedeveloper@gmail.com>
2025-11-12 09:42:30 +0800

    Introduce Multithreading to video_downloader.py

--- start_kit/video_downloader.py
+++ start_kit/video_downloader.py
@@ -1,13 +1,8 @@
-import os
-import json
-import time
-import sys
+import os, json, time, sys, random, logging
 import urllib.request
 from multiprocessing.dummy import Pool
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-import random
-
-import logging
 logging.basicConfig(filename='download_{}.log'.format(int(time.time())), filemode='w', level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
@@ -74,32 +69,17 @@ def select_download_method(url):
         return download_others
 
 
-def download_nonyt_videos(indexfile, saveto='raw_videos'):
-    content = json.load(open(indexfile))
+def download_nonyt_videos(saveto, gloss, video_id, video_url):
+    logging.info('gloss: {}, video: {}.'.format(gloss, video_id))
 
-    if not os.path.exists(saveto):
-        os.mkdir(saveto)
-
-    for entry in content:
-        gloss = entry['gloss']
-        instances = entry['instances']
-
-        for inst in instances:
-            video_url = inst['url']
-            video_id = inst['video_id']
-            
-            logging.info('gloss: {}, video: {}.'.format(gloss, video_id))
-
-            download_method = select_download_method(video_url)    
-            
-            if download_method == download_youtube:
-                logging.warning('Skipping YouTube video {}'.format(video_id))
-                continue
-
-            try:
-                download_method(video_url, saveto, video_id)
-            except Exception as e:
-                logging.error('Unsuccessful downloading - video {}'.format(video_id))
+    download_method = select_download_method(video_url)
+    if download_method == download_youtube:
+        logging.warning('Skipping YouTube video {}'.format(video_id))
+    else:
+        try:
+            download_method(video_url, saveto, video_id)
+        except Exception as e:
+            logging.error('Unsuccessful downloading - video {}'.format(video_id))
 
 
 def check_youtube_dl_version():
@@ -108,46 +88,54 @@ def check_youtube_dl_version():
     assert ver, f"{youtube_downloader} cannot be found in PATH. Please verify your installation."
 
 
-def download_yt_videos(indexfile, saveto='raw_videos'):
-    content = json.load(open(indexfile))
+def download_yt_videos(saveto, video_url):
+    if os.path.exists(os.path.join(saveto, video_url[-11:] + '.mp4')) or os.path.exists(os.path.join(saveto, video_url[-11:] + '.mkv')):
+        logging.info('YouTube videos {} already exists.'.format(video_url))
+    else:
+        cmd = f"{youtube_downloader} \"{{}}\" -o \"{{}}%(id)s.%(ext)s\""
+        cmd = cmd.format(video_url, saveto + os.path.sep)
+
+        rv = os.system(cmd)
+        
+        if not rv:
+            logging.info('Finish downloading youtube video url {}'.format(video_url))
+        else:
+            logging.error('Unsuccessful downloading - youtube video url {}'.format(video_url))
+
+        # please be nice to the host - take pauses and avoid spamming
+        time.sleep(random.uniform(1.0, 1.5))
     
-    if not os.path.exists(saveto):
-        os.mkdir(saveto)
-    
-    for entry in content:
-        gloss = entry['gloss']
-        instances = entry['instances']
 
-        for inst in instances:
-            video_url = inst['url']
-            video_id = inst['video_id']
+def process_video(gloss, video_url, video_id, saveto):
+    if 'youtube' not in video_url and 'youtu.be' not in video_url:
+        download_nonyt_videos(saveto, gloss, video_id, video_url)
+    else:
+        download_yt_videos(saveto, video_url)
 
-            if 'youtube' not in video_url and 'youtu.be' not in video_url:
-                continue
-
-            if os.path.exists(os.path.join(saveto, video_url[-11:] + '.mp4')) or os.path.exists(os.path.join(saveto, video_url[-11:] + '.mkv')):
-                logging.info('YouTube videos {} already exists.'.format(video_url))
-                continue
-            else:
-                cmd = f"{youtube_downloader} \"{{}}\" -o \"{{}}%(id)s.%(ext)s\""
-                cmd = cmd.format(video_url, saveto + os.path.sep)
-
-                rv = os.system(cmd)
-                
-                if not rv:
-                    logging.info('Finish downloading youtube video url {}'.format(video_url))
-                else:
-                    logging.error('Unsuccessful downloading - youtube video url {}'.format(video_url))
-
-                # please be nice to the host - take pauses and avoid spamming
-                time.sleep(random.uniform(1.0, 1.5))
-    
 
 if __name__ == '__main__':
-    logging.info('Start downloading non-youtube videos.')
-    download_nonyt_videos('WLASL_v0.3.json')
+    indexfile = 'WLASL_v0.3.json'
+    saveto = 'raw_videos'
+    os.makedirs(saveto, exist_ok=True)
 
-    check_youtube_dl_version()
-    logging.info('Start downloading youtube videos.')
-    download_yt_videos('WLASL_v0.3.json')
+    content = json.load(open(indexfile))
+    logging.info('Start downloading videos using multithreading.')
 
+    MAX_WORKERS = 8
+    futures = []
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        for entry in content:
+            gloss = entry['gloss']
+            for inst in entry['instances']:
+                video_url = inst['url']
+                video_id = inst['video_id']
+                futures.append(executor.submit(process_video, gloss, video_url, video_id, saveto))
+
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                logging.error(f"Error: {e}")
+
+    logging.info('All downloads completed.')
